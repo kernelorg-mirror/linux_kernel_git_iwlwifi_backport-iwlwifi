4d6c450b46c6f4edd4ae2fa76b7330e987ed53f9
Johannes Berg <johannes.berg@intel.com>
2025-04-03 12:41:51 +0300

    [BUGFIX] wifi: iwlwifi: pcie: set state to no-FW before reset handshake
    The reset handshake attempts to kill the firmware, and it'll go
    into a pretty much dead state once we do that. However, if it
    times out, then we'll attempt to dump the firmware to be able
    to see why it didn't respond. During this dump, we cannot treat
    it as if it was still running, since we just tried to kill it,
    otherwise dumping will attempt to send a DBGC stop command. As
    this command will time out, we'll go into a reset loop.
    
    For now, fix this by setting the trans->state to say firmware
    isn't running before doing the reset handshake. In the longer
    term, we should clean up the way this state is handled.
    
    It's not entirely clear but it seems likely that this issue was
    introduced by my rework of the error handling, prior to that it
    would've been synchronous at that point and (I think) not have
    attempted to reset since it was already doing down.
    
    type=bugfix
    fixes=I96a9243e7877808ed6d1bff6967c15d6c24882f0
    ticket=jira:WIFI-636946
    
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=219967
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=219968
    Fixes: 7391b2a4f7db ("wifi: iwlwifi: rework firmware error handling")
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Change-Id: Ife70cfe997db03f0d07fdef2b164695739a05a63
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/187205
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: eb4b045c3e4132b900d885947909cf02021c9446

--- drivers/net/wireless/intel/iwlwifi/pcie/trans-gen2.c
+++ drivers/net/wireless/intel/iwlwifi/pcie/trans-gen2.c
@@ -157,8 +157,14 @@ static void _iwl_trans_pcie_gen2_stop_device(struct iwl_trans *trans)
 		return;
 
 	if (trans->state >= IWL_TRANS_FW_STARTED &&
-	    trans->conf.fw_reset_handshake)
+	    trans->conf.fw_reset_handshake) {
+		/*
+		 * Reset handshake can dump firmware on timeout, but that
+		 * should assume that the firmware is already dead.
+		 */
+		trans->state = IWL_TRANS_NO_FW;
 		iwl_trans_pcie_fw_reset_handshake(trans);
+	}
 
 	trans_pcie->is_down = true;
 	trans_pcie->alive_isr_received = false;
