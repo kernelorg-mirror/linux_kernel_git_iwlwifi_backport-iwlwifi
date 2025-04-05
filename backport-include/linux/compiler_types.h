d436e784efb9beabc8942ef698fd99453ab76571
Gustavo A. R. Silva <gustavoars@kernel.org>
2025-04-05 12:07:26 -0600

    wifi: iwlwifi: mvm: d3: Avoid -Wflex-array-member-not-at-end warnings
    -Wflex-array-member-not-at-end was introduced in GCC-14, and we are
    getting ready to enable it, globally.
    
    Use the `DEFINE_RAW_FLEX()` helper for on-stack definitions of
    a flexible structure where the size of the flexible-array member
    is known at compile-time, and refactor the rest of the code,
    accordingly.
    
    So, with these changes, fix the following warnings:
    
    drivers/net/wireless/intel/iwlwifi/mvm/d3.c:124:52: warning: structure containing a flexible array member is not at the end of another structure [-Wflex-array-member-not-at-end]
    drivers/net/wireless/intel/iwlwifi/mvm/d3.c:2067:51: warning: structure containing a flexible array member is not at the end of another structure [-Wflex-array-member-not-at-end]
    drivers/net/wireless/intel/iwlwifi/mvm/d3.c:2162:43: warning: structure containing a flexible array member is not at the end of another structure [-Wflex-array-member-not-at-end]
    drivers/net/wireless/intel/iwlwifi/mvm/d3.c:2225:43: warning: structure containing a flexible array member is not at the end of another structure [-Wflex-array-member-not-at-end]
    
    type=feature
    ticket=none
    
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Reviewed-by: Kees Cook <kees@kernel.org>
    Link: https://msgid.link/Z_FxXjiMvG5u73fi@kspp
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: I99bb09b826f28aa72b6f1291f0da5dd01c62d75c
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/193420
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: 7322a1737d1da13c86ab2b6c6e85e12a69a08d4c

---
+++ backport-include/linux/compiler_types.h
@@ -0,0 +1,21 @@
+#ifndef _BACKPORTS_LINUX_COMPILER_TYPES_H
+#define _BACKPORTS_LINUX_COMPILER_TYPES_H 1
+#include_next <linux/compiler_types.h>
+
+#ifndef __has_builtin
+#define __has_builtin(x) (0)
+#endif
+
+/*
+ * When the size of an allocated object is needed, use the best available
+ * mechanism to find it. (For cases where sizeof() cannot be used.)
+ */
+#if __has_builtin(__builtin_dynamic_object_size)
+#define __struct_size(p)	__builtin_dynamic_object_size(p, 0)
+#define __member_size(p)	__builtin_dynamic_object_size(p, 1)
+#else
+#define __struct_size(p)	__builtin_object_size(p, 0)
+#define __member_size(p)	__builtin_object_size(p, 1)
+#endif
+
+#endif /* _BACKPORTS_LINUX_COMPILER_TYPES_H */
