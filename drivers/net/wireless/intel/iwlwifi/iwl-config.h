955b9b87483989698dd429612a7fd519d1979bf1
Johannes Berg <johannes.berg@intel.com>
2025-04-09 20:30:08 +0300

    wifi: iwlwifi: cfg: add FM RF config
    The Bz configs really should be FM for the RF, so
    move that around.
    
    type=cleanup
    ticket=none
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Change-Id: I6608adf28ba601dee75194c405384754ce229e81
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/188705
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 2ec33a9e7f9168b5f4564054c9e2ebad7c3d40b3

--- drivers/net/wireless/intel/iwlwifi/iwl-config.h
+++ drivers/net/wireless/intel/iwlwifi/iwl-config.h
@@ -665,8 +665,8 @@ extern const struct iwl_cfg iwl_rf_gf;
 #endif /* CPTCFG_IWLMVM */
 
 #if IS_ENABLED(CPTCFG_IWLMLD)
-extern const struct iwl_cfg iwl_cfg_bz;
-extern const struct iwl_cfg iwl_cfg_bz_160mhz;
+extern const struct iwl_cfg iwl_rf_fm;
+extern const struct iwl_cfg iwl_rf_fm_160mhz;
 
 extern const struct iwl_cfg iwl_cfg_sc;
 extern const struct iwl_cfg iwl_cfg_sc_160mhz;
