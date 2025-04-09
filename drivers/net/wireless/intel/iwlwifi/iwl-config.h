ac0e743bec68b22f2f53f16b82bac8073a1edcbc
Johannes Berg <johannes.berg@intel.com>
2025-04-09 22:13:49 +0300

    wifi: iwlwifi: cfg: unify and add some Killer devices
    Unify a number of Killer devices now that we no longer
    need to distinguish the MAC type, and add a few more
    that wouldn't have gotten the right name before.
    
    type=cleanup
    ticket=none
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Change-Id: I147b97ef2c8e99451806ea0e34a9eb5bff37c326
    x-iwlwifi-stack-dev: ce41976973d430849580c9bd9d640a2d8bd0bf04

--- drivers/net/wireless/intel/iwlwifi/iwl-config.h
+++ drivers/net/wireless/intel/iwlwifi/iwl-config.h
@@ -645,6 +645,10 @@ extern const char iwl_gl_name[];
 extern const char iwl_mtp_name[];
 extern const char iwl_dr_name[];
 extern const char iwl_br_name[];
+extern const char iwl_killer_be1750s_name[];
+extern const char iwl_killer_be1750i_name[];
+extern const char iwl_killer_be1750w_name[];
+extern const char iwl_killer_be1750x_name[];
 #if IS_ENABLED(CPTCFG_IWLMVM)
 extern const struct iwl_rf_cfg iwl7260_cfg;
 extern const struct iwl_rf_cfg iwl7260_high_temp_cfg;
