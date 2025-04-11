183483642fcb14ef541bbf7e7cfae7abe875da9e
Johannes Berg <johannes.berg@intel.com>
2025-04-11 10:41:50 +0300

    wifi: iwlwifi: cfg: clean up dr/br configs
    We don't need the configs that won't end up being used, such as
    the "br" config for discrete devices, remove them. Also remove
    the module firmware for test chips, that's never needed.
    
    For now keep the iwl_dr_mac_cfg even if it's unused, we'll add
    platforms with it once we have their PCI IDs.
    
    type=cleanup
    ticket=none
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Change-Id: I75a6ce4ad0b14d2b4413615f05523a8f62f08954
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/189099
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: cf2a4c7aa4d1b3cf403a61cf67cdc4d317c10c1b

--- drivers/net/wireless/intel/iwlwifi/iwl-config.h
+++ drivers/net/wireless/intel/iwlwifi/iwl-config.h
@@ -536,7 +536,6 @@ extern const struct iwl_mac_cfg iwl_bz_mac_cfg;
 extern const struct iwl_mac_cfg iwl_gl_mac_cfg;
 extern const struct iwl_mac_cfg iwl_sc_mac_cfg;
 extern const struct iwl_mac_cfg iwl_dr_mac_cfg;
-extern const struct iwl_mac_cfg iwl_br_mac_cfg;
 
 extern const char iwl1000_bgn_name[];
 extern const char iwl1000_bg_name[];
@@ -653,8 +652,6 @@ extern const char iwl_killer_bn1850i_name[];
 extern const char iwl_bn201_name[];
 extern const char iwl_be221_name[];
 extern const char iwl_be223_name[];
-extern const char iwl_dr_name[];
-extern const char iwl_br_name[];
 #if IS_ENABLED(CPTCFG_IWLMVM)
 extern const struct iwl_rf_cfg iwl7260_cfg;
 extern const struct iwl_rf_cfg iwl7260_high_temp_cfg;
