55f1266674cd6473b3433dfd7edcb19562b92c8a
Johannes Berg <johannes.berg@intel.com>
2025-04-24 15:08:19 +0200

    wifi: iwlwifi: mvm/mld: allow puncturing use in 5 GHz
    It was decided this was supported after all, so remove
    the restriction.
    
    type=feature
    ticket=jira:WIFI-654989
    
    Change-Id: I4f1f1383a894f3035623274b310b12c916da3f09
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/192023
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 1e1454d05cce908606d49c204058cf372fb8a7ec

--- drivers/net/wireless/intel/iwlwifi/mld/mac80211.c
+++ drivers/net/wireless/intel/iwlwifi/mld/mac80211.c
@@ -288,7 +288,6 @@ static void iwl_mac_hw_set_flags(struct iwl_mld *mld)
 	ieee80211_hw_set(hw, TX_AMPDU_SETUP_IN_HW);
 	ieee80211_hw_set(hw, HAS_RATE_CONTROL);
 	ieee80211_hw_set(hw, SUPPORTS_REORDERING_BUFFER);
-	ieee80211_hw_set(hw, DISALLOW_PUNCTURING_5GHZ);
 	ieee80211_hw_set(hw, SINGLE_SCAN_ON_ALL_BANDS);
 #ifdef CPTCFG_IWLWIFI_SUPPORT_DEBUG_OVERRIDES
 	if (!mld->trans->dbg_cfg.amsdu_in_ampdu_disabled)
