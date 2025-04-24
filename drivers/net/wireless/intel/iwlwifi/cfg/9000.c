9f9a7ab4ace304548ae301101208b631b0a2508c
Johannes Berg <johannes.berg@intel.com>
2025-04-24 13:02:51 +0200

    [BUGFIX] wifi: iwlwifi: cfg: add EEPROM size for 9000
    Evidently, I was mistaken about 9000 series NVM parsing.
    Add the EEPROM size back for those devices.
    
    type=bugfix
    fixes=I1deccbaebe2cb3d89fdd7e8e27d1777bf480bb6a
    ticket=none
    
    Change-Id: I6a86e4a12effea33d8d58d37dbb77646e8d7ba63
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/191993
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: 522d2f8d3715242d2bc14f65b9e2264967aa3c18

--- drivers/net/wireless/intel/iwlwifi/cfg/9000.c
+++ drivers/net/wireless/intel/iwlwifi/cfg/9000.c
@@ -34,6 +34,7 @@
 	IWL9260_FW_PRE "-" __stringify(api) ".ucode"
 
 static const struct iwl_family_base_params iwl9000_base = {
+	.eeprom_size = OTP_LOW_IMAGE_SIZE_32K,
 	.num_of_queues = 31,
 	.max_tfd_queue_size = 256,
 	.shadow_ram_support = true,
