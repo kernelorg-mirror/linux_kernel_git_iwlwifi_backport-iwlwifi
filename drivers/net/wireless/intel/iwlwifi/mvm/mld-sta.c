acaedf64e83137cc6c73bde7c237d0034c7e6eb5
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2025-05-05 09:27:52 +0300

    [BUGFIX] wifi: iwlwifi: mvm: correctly lookup cmd version
    The version of the STA cmd should be looked up by the WIDE_ID, including
    the cmd group.
    
    type=bugfix
    ticket=none
    fixes=If2ff606e20ccf83e9b27ca2f81686108a829896a
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: I4dcd2e4e023c1e9f02973bf79de6617ca41b6969
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/194437
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Yedidya Ben Shimol <yedidya.ben.shimol@intel.com>
    x-iwlwifi-stack-dev: 40645ed7bf81dbb855e768c31e8b8062dff9244a

--- drivers/net/wireless/intel/iwlwifi/mvm/mld-sta.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/mld-sta.c
@@ -48,7 +48,8 @@ u32 iwl_mvm_sta_fw_id_mask(struct iwl_mvm *mvm, struct ieee80211_sta *sta,
 static int iwl_mvm_mld_send_sta_cmd(struct iwl_mvm *mvm,
 				    struct iwl_sta_cfg_cmd *cmd)
 {
-	int cmd_len = iwl_fw_lookup_cmd_ver(mvm->fw, STA_CONFIG_CMD, 0) > 1 ?
+	u32 cmd_id = WIDE_ID(MAC_CONF_GROUP, STA_CONFIG_CMD);
+	int cmd_len = iwl_fw_lookup_cmd_ver(mvm->fw, cmd_id, 0) > 1 ?
 		      sizeof(*cmd) :
 		      sizeof(struct iwl_sta_cfg_cmd_v1);
 	int ret = iwl_mvm_send_cmd_pdu(mvm,
