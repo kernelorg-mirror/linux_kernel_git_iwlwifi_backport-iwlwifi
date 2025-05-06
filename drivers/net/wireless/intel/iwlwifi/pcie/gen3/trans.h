f06c4c7b1a859990678c07d80bfc7272900e2b28
Yedidya Benshimol <yedidya.ben.shimol@intel.com>
2025-05-06 12:08:12 +0300

    wifi: iwlwifi: pcie: add a pcie gen3 getter
    type=feature
    ticket=none
    
    Signed-off-by: Yedidya Benshimol <yedidya.ben.shimol@intel.com>
    Change-Id: Ic1f92e698a8d28c9d0438deebb96164404bbdaa4
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/194762
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: fd03f4101a89e93e1a8d2aa318a8c1efdaf1a679

--- drivers/net/wireless/intel/iwlwifi/pcie/gen3/trans.h
+++ drivers/net/wireless/intel/iwlwifi/pcie/gen3/trans.h
@@ -17,3 +17,9 @@ struct iwl_pcie_gen3 {
 int iwl_pci_gen3_probe(struct pci_dev *pdev,
 		       const struct pci_device_id *ent,
 		       const struct iwl_mac_cfg *mac_cfg);
+
+static inline struct iwl_pcie_gen3 *
+IWL_GET_PCIE_GEN3(struct iwl_trans *trans)
+{
+	return (void *)trans->trans_specific;
+}
