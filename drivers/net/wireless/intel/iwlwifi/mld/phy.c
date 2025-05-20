91a1d8bd20945ab597c4c6ea020df0558107d0d7
Johannes Berg <johannes.berg@intel.com>
2025-05-20 14:12:58 +0200

    wifi: iwlwifi: mld: make PHY config a debug message
    This means nothing to a normal user and really has no value
    for most people, print it as a debug message instead.
    
    type=cleanup
    ticket=none
    fixes=I0ed29d461504aea00519bdd2e65430405f169238
    
    Change-Id: I2cf4e1e2e604b42b6eb9737c0ef3b75fec69edea
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/198385
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: cd4f89f57c3b0cbf4f6f13e122acd5e61953f9a4

--- drivers/net/wireless/intel/iwlwifi/mld/phy.c
+++ drivers/net/wireless/intel/iwlwifi/mld/phy.c
@@ -190,7 +190,7 @@ int iwl_mld_send_phy_cfg_cmd(struct iwl_mld *mld)
 	}
 #endif
 
-	IWL_INFO(mld, "Sending Phy CFG command: 0x%x\n", cmd.phy_cfg);
+	IWL_DEBUG_INFO(mld, "Sending Phy CFG command: 0x%x\n", cmd.phy_cfg);
 
 	return iwl_mld_send_cmd_pdu(mld, PHY_CONFIGURATION_CMD, &cmd);
 }
