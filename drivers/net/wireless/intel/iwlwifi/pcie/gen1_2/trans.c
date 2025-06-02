d7431cea09648cf1207d1a4ed4b955342eed6ff6
Johannes Berg <johannes.berg@intel.com>
2025-06-02 14:08:47 +0200

    wifi: iwlwifi: pcie: rename iwl_pci_gen1_2_probe() argument
    Using 'trans' for the mac config is confusing, rename the
    argument to 'mac_cfg'.
    
    type=cleanup
    ticket=none
    
    Change-Id: I8b39f01e06ad7791efe718c267cbf367233920a3
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/201515
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 99ef793c066c30dd6299ca04cff8ac2107c43e64

--- drivers/net/wireless/intel/iwlwifi/pcie/gen1_2/trans.c
+++ drivers/net/wireless/intel/iwlwifi/pcie/gen1_2/trans.c
@@ -4180,8 +4180,8 @@ static void iwl_pcie_check_me_status(struct iwl_trans *trans)
 
 int iwl_pci_gen1_2_probe(struct pci_dev *pdev,
 			 const struct pci_device_id *ent,
-			 const struct iwl_mac_cfg *trans, u8 __iomem *hw_base,
-			 u32 hw_rev)
+			 const struct iwl_mac_cfg *mac_cfg,
+			 u8 __iomem *hw_base, u32 hw_rev)
 {
 	const struct iwl_dev_info *dev_info;
 	struct iwl_trans_info info = {
@@ -4195,7 +4195,7 @@ int iwl_pci_gen1_2_probe(struct pci_dev *pdev,
 #if IS_ENABLED(CPTCFG_IWLMVM)
 #endif
 
-	iwl_trans = iwl_trans_pcie_alloc(pdev, trans, &info, hw_base);
+	iwl_trans = iwl_trans_pcie_alloc(pdev, mac_cfg, &info, hw_base);
 	if (IS_ERR(iwl_trans))
 		return PTR_ERR(iwl_trans);
 
