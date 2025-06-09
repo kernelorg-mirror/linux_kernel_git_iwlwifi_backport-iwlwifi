7b57074d2a6f5cafa62b90ddf07da6b6cb77a517
Ilan Peer <ilan.peer@intel.com>
2025-06-09 08:21:12 +0300

    wifi: cfg80211: Support Tx/Rx of action frame for NAN
    Add support for sending and receiving action frames over a NAN Device
    interface:
    
    - For Synchronized NAN operation NAN Service Discovery
      Frames (SDFs) and NAN Action Frames (NAFs) transmissions
      over a NAN Device interface, a channel parameter is not
      mandatory as the frame can be transmitted based on the NAN
      Device schedule.
    - For Unsynchronized NAN Discovery (USD) operation the
      SDFs and NAFs could be transmitted using NL80211_CMD_FRAME
      where a specific channel and dwell time are configured.
    
    As Synchronized NAN Operation and USD can be done concurrently,
    both modes need to be supported. Thus, allow sending NAN action
    frames when user space handles the NAN Discovery Engine (DE) with
    and without providing a channel as a parameter.
    
    To support reception of NAN Action frames and Authentication
    frames (used for NAN paring and verification) allow to
    register for management frame reception of NAN Device interface
    when user space handles the NAN DE.
    
    type=feature
    ticket=jira:WIFI-663098
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Change-Id: I0166d51dcf14393f628cd5da366c21114f518618
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/203119
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: c67e6a17169dcda29bba51195bd4bb4e83fdcdc9

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -12900,7 +12900,9 @@ static int nl80211_register_mgmt(struct sk_buff *skb, struct genl_info *info)
 		break;
 	case NL80211_IFTYPE_NAN:
 		if (!wiphy_ext_feature_isset(wdev->wiphy,
-					     NL80211_EXT_FEATURE_SECURE_NAN))
+					     NL80211_EXT_FEATURE_SECURE_NAN) &&
+		    !(wdev->wiphy->nan_capa.flags &
+		      WIPHY_NAN_FLAGS_USERSPACE_DE))
 			return -EOPNOTSUPP;
 		break;
 	default:
@@ -12961,7 +12963,9 @@ static int nl80211_tx_mgmt(struct sk_buff *skb, struct genl_info *info)
 		break;
 	case NL80211_IFTYPE_NAN:
 		if (!wiphy_ext_feature_isset(wdev->wiphy,
-					     NL80211_EXT_FEATURE_SECURE_NAN))
+					     NL80211_EXT_FEATURE_SECURE_NAN) &&
+		    !(wdev->wiphy->nan_capa.flags &
+		      WIPHY_NAN_FLAGS_USERSPACE_DE))
 			return -EOPNOTSUPP;
 		break;
 	default:
