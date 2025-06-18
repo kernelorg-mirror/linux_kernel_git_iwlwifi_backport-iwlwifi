f880cd69af072e9628e574ad27d4a4833cd3562b
Pagadala Yesu Anjaneyulu <pagadala.yesu.anjaneyulu@intel.com>
2025-06-18 06:36:02 +0300

    wifi: iwlwifi: add support for accepting raw DSM tables by firmware
    Firmware would assert on undefined bits in DSM-originated DWs.
    With this change, Firmware introduces a fail-safe mechanism
    and removes the assert behavior. This ensures robustness when
    handling raw DSM table data.
    
    type=feature
    ticket=jira:WIFI-690680
    ticket=jira:WIFI-623080
    
    Signed-off-by: Pagadala Yesu Anjaneyulu <pagadala.yesu.anjaneyulu@intel.com>
    Change-Id: I2be2eaa16437e84aa1be0c6c95ec334034ce7e50
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/205569
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 85072e78554bd6d3ce6f7bf13801ea8ea095354c

--- drivers/net/wireless/intel/iwlwifi/fw/file.h
+++ drivers/net/wireless/intel/iwlwifi/fw/file.h
@@ -421,6 +421,8 @@ typedef unsigned int __bitwise iwl_ucode_tlv_capa_t;
  * @IWL_UCODE_TLV_CAPA_EXT_FSEQ_IMAGE_SUPPORT: external FSEQ image support
  * @IWL_UCODE_TLV_CAPA_RESTRICTED_TWT_SUPPORT: Supports restricted TWT operation
  * @IWL_UCODE_TLV_CAPA_TOF_320MHZ_SUPPORT: Supports 320 MHz ranging
+ * @IWL_UCODE_TLV_CAPA_FW_ACCEPTS_RAW_DSM_TABLE: Firmware has capability of
+ *	handling raw DSM table data.
  * @IWL_UCODE_TLV_CAPA_NAN_SYNC_SUPPORT: Supports NAN synchronization
  *
  * @NUM_IWL_UCODE_TLV_CAPA: number of bits used
@@ -537,9 +539,9 @@ enum iwl_ucode_tlv_capa {
 	 * @IWL_UCODE_TLV_CAPA_RESET_DURING_ASSERT: FW reset handshake is needed
 	 *	during assert handling even if the dump isn't split
 	 */
-	IWL_UCODE_TLV_CAPA_RESET_DURING_ASSERT	= (__force iwl_ucode_tlv_capa_t)(4 * 32 + 0),
-	IWL_UCODE_TLV_CAPA_NAN_SYNC_SUPPORT	= (__force iwl_ucode_tlv_capa_t)(4 * 32 + 2),
-
+	IWL_UCODE_TLV_CAPA_RESET_DURING_ASSERT		= (__force iwl_ucode_tlv_capa_t)(4 * 32 + 0),
+	IWL_UCODE_TLV_CAPA_FW_ACCEPTS_RAW_DSM_TABLE 	= (__force iwl_ucode_tlv_capa_t)(4 * 32 + 1),
+	IWL_UCODE_TLV_CAPA_NAN_SYNC_SUPPORT		= (__force iwl_ucode_tlv_capa_t)(4 * 32 + 2),
 	NUM_IWL_UCODE_TLV_CAPA
 /*
  * This construction make both sparse (which cannot increment the previous
