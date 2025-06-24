97a2ddc7247cad15d8d27f2a23313aaef014ce4f
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2025-06-24 08:37:35 +0300

    [BUGFIX][NOUPSTREAM] wifi: iwlwifi: don't enable TWT by default
    TWT is not ready yet to be enabled by default.
    We keep having issues with this, so instead of enabling it on master
    branch and disabling it on core branches, disable it on master by
    default and allow to enable it only when needed .
    MLD_USE_TWT is no longer needed.
    
    type=bugfix
    ticket=none
    fixes=unknown
    
    Change-Id: I6ff1f72b096c70d2c024fc2eca2cbaffaebaa7ad
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/206656
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: c11365e3f833d042d9d77455e457fdc982a276e0

--- drivers/net/wireless/intel/iwlwifi/iwl-nvm-parse.c
+++ drivers/net/wireless/intel/iwlwifi/iwl-nvm-parse.c
@@ -624,8 +624,7 @@ static const struct ieee80211_sband_iftype_data iwl_he_eht_capa[] = {
 			.has_he = true,
 			.he_cap_elem = {
 				.mac_cap_info[0] =
-					IEEE80211_HE_MAC_CAP0_HTC_HE |
-					IEEE80211_HE_MAC_CAP0_TWT_REQ,
+					IEEE80211_HE_MAC_CAP0_HTC_HE,
 				.mac_cap_info[1] =
 					IEEE80211_HE_MAC_CAP1_TF_MAC_PAD_DUR_16US |
 					IEEE80211_HE_MAC_CAP1_MULTI_TID_AGG_RX_QOS_8,
@@ -1091,6 +1090,12 @@ iwl_nvm_fixup_sband_iftd(struct iwl_trans *trans,
 	if (fw_has_capa(&fw->ucode_capa, IWL_UCODE_TLV_CAPA_RESTRICTED_TWT_SUPPORT))
 		iftype_data->eht_cap.eht_cap_elem.mac_cap_info[0] |=
 			IEEE80211_EHT_MAC_CAP0_RESTRICTED_TWT;
+
+#ifdef CPTCFG_IWLWIFI_SUPPORT_DEBUG_OVERRIDES
+	if (trans->dbg_cfg.allow_twt)
+		iftype_data->he_cap.he_cap_elem.mac_cap_info[0] |=
+			IEEE80211_HE_MAC_CAP0_TWT_REQ;
+#endif
 }
 
 static void iwl_init_he_hw_capab(struct iwl_trans *trans,
