ffd6276d063ce248d3b00803cdab00639d0d7ae4
Ilan Peer <ilan.peer@intel.com>
2025-07-01 03:29:00 +0300

    wifi: mac80211: Get the correct interface for non-netdev skb status
    The function ieee80211_sdata_from_skb() always returned the P2P Device
    interface in case the skb was not associated with a netdev and didn't
    consider the possibility that an NAN Device interface is also enabled.
    
    To support configurations where both P2P Device and a NAN Device
    interface are active, extend the function to match the correct
    interface based on address 2 in the 802.11 MAC header.
    
    Since the 'p2p_sdata' field in struct ieee80211_local is no longer
    needed, remove it.
    
    type=feature
    ticket=none
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Change-Id: Id4576531c6b2ad83c9498b708dc0ade6b0214fa8
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/208271
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    Reviewed-by: Andrei Otcheretianski <andrei.otcheretianski@intel.com>
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: 7a55d4bf210cc43251008df9921667c5983abb46

--- net/mac80211/iface.c
+++ net/mac80211/iface.c
@@ -615,10 +615,6 @@ static void ieee80211_do_stop(struct ieee80211_sub_if_data *sdata, bool going_do
 		break;
 	case NL80211_IFTYPE_NAN:
 		break;
-	case NL80211_IFTYPE_P2P_DEVICE:
-		/* relies on synchronize_rcu() below */
-		RCU_INIT_POINTER(local->p2p_sdata, NULL);
-		fallthrough;
 	default:
 		wiphy_work_cancel(sdata->local->hw.wiphy, &sdata->work);
 		/*
@@ -1437,6 +1433,7 @@ int ieee80211_do_open(struct wireless_dev *wdev, bool coming_up)
 		ieee80211_recalc_idle(local);
 
 		netif_carrier_on(dev);
+		list_add_tail_rcu(&sdata->u.mntr.list, &local->mon_list);
 		break;
 	default:
 		if (coming_up) {
@@ -1498,17 +1495,6 @@ int ieee80211_do_open(struct wireless_dev *wdev, bool coming_up)
 		 */
 		ieee80211_set_wmm_default(&sdata->deflink, true,
 			sdata->vif.type != NL80211_IFTYPE_STATION);
-	}
-
-	switch (sdata->vif.type) {
-	case NL80211_IFTYPE_P2P_DEVICE:
-		rcu_assign_pointer(local->p2p_sdata, sdata);
-		break;
-	case NL80211_IFTYPE_MONITOR:
-		list_add_tail_rcu(&sdata->u.mntr.list, &local->mon_list);
-		break;
-	default:
-		break;
 	}
 
 	/*
