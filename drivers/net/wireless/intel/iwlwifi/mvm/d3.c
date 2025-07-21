d1676d25a2829dc9ea12d2efdfc44a6d70ac1d03
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2025-07-21 09:53:08 +0300

    [BUGFIX] wifi: iwlwifi: mvm: don't copy the key in GTK rekey
    There is no need to copy the key to an intermediate buffer.
    We can send to ieee80211_gtk_rekey_add the key data directly from the
    stored key.
    
    type=bugfix
    ticket=none
    fixes=I5d92258a9d63a39ee3acb02a72a2af9984993018
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: I60dcf19920007c07cd571e7090d854cc2d92f9a3
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/212847
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    x-iwlwifi-stack-dev: e01c78d8e798895bc49f9f054addeef7bc7ebd2c

--- drivers/net/wireless/intel/iwlwifi/mvm/d3.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/d3.c
@@ -1894,9 +1894,6 @@ static bool iwl_mvm_gtk_rekey(struct iwl_wowlan_status_data *status,
 	int i, j;
 	struct ieee80211_key_conf *key;
 	int link_id = vif->active_links ? __ffs(vif->active_links) : -1;
-	u8 key_data[WOWLAN_KEY_MAX_SIZE];
-
-	BUILD_BUG_ON(sizeof(key_data) != sizeof(status->gtk[0].key));
 
 	for (i = 0; i < ARRAY_SIZE(status->gtk); i++) {
 		if (!status->gtk[i].len)
@@ -1906,10 +1903,10 @@ static bool iwl_mvm_gtk_rekey(struct iwl_wowlan_status_data *status,
 				 "Received from FW GTK: key index %d\n",
 				 status->gtk[i].id);
 
-		memcpy(key_data, status->gtk[i].key, sizeof(status->gtk[i].key));
-
-		key = ieee80211_gtk_rekey_add(vif, status->gtk[i].id, key_data,
-					      sizeof(key_data), link_id);
+		key = ieee80211_gtk_rekey_add(vif, status->gtk[i].id,
+					      status->gtk[i].key,
+					      sizeof(status->gtk[i].key),
+					      link_id);
 		if (IS_ERR(key)) {
 			/* FW may send also the old keys */
 			if (PTR_ERR(key) == -EALREADY)
@@ -1939,17 +1936,13 @@ iwl_mvm_d3_igtk_bigtk_rekey_add(struct iwl_wowlan_status_data *status,
 	struct ieee80211_key_conf *key_config;
 	struct ieee80211_key_seq seq;
 	int link_id = vif->active_links ? __ffs(vif->active_links) : -1;
-	u8 key[WOWLAN_KEY_MAX_SIZE];
 	s8 keyidx = key_data->id;
 
 	if (!key_data->len)
 		return true;
 
-	BUILD_BUG_ON(sizeof(key_data->key) != WOWLAN_KEY_MAX_SIZE);
-	memcpy(key, key_data->key, sizeof(key_data->key));
-
-	key_config = ieee80211_gtk_rekey_add(vif, keyidx, key, sizeof(key),
-					     link_id);
+	key_config = ieee80211_gtk_rekey_add(vif, keyidx, key_data->key,
+					     sizeof(key_data->key), link_id);
 	if (IS_ERR(key_config)) {
 		/* FW may send also the old keys */
 		return PTR_ERR(key_config) == -EALREADY;
