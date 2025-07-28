ef33215b41adf9d5a286f601d96c09b27daa2691
Johannes Berg <johannes.berg@intel.com>
2025-07-28 16:05:46 +0200

    wifi: iwlwifi: bump core version for BZ/SC/DR
    Start supporting Core 100 FW on those devices.
    
    type=feature
    ticket=none
    
    Change-Id: Idd6281cd647f1b33f2572a8c107c3a3228e03665
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/214727
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: bad8f2be4b12684a3ca7d6ab79a160ab7b82fcb8

--- drivers/net/wireless/intel/iwlwifi/iwl-drv.c
+++ drivers/net/wireless/intel/iwlwifi/iwl-drv.c
@@ -559,9 +559,8 @@ static int iwl_request_firmware(struct iwl_drv *drv, bool first)
 
 	if (first)
 		drv->fw_index = ucode_api_max;
-	else if (drv->fw_index == ENCODE_CORE_AS_API(99))
-		/* FIXME: change to 101 once FW is updated */
-		drv->fw_index = 102;
+	else if (drv->fw_index == ENCODE_CORE_AS_API(100))
+		drv->fw_index = 102; /* last API-scheme number below core 100 */
 	else
 		drv->fw_index--;
 
