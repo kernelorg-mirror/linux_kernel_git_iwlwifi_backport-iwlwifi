13be09f21f2d60f185d10c68b72c182a23f1c741
Benjamin Berg <benjamin.berg@intel.com>
2025-08-26 11:02:36 +0200

    [BUGFIX] wifi: iwlwifi: mld: add back monitor PTP adjustment
    The feature was accidentally dropped in the port to the new sniffer API.
    Add back the adjustment code so that it is again possible to use the PTP
    adjusted time for the monitor.
    
    type=bugfix
    ticket=none
    fixes=I4a18b2043703c1f9a8f55c108dcaaeca7891e19c
    
    Change-Id: Iac4e7709db955ceef99b839fa1612cf98cfd0b8f
    Signed-off-by: Benjamin Berg <benjamin.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/221539
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: a54c0f161ebb24dd34299d0d4c95423d78ccdd8f

--- drivers/net/wireless/intel/iwlwifi/mld/rx.c
+++ drivers/net/wireless/intel/iwlwifi/mld/rx.c
@@ -1502,8 +1502,20 @@ static void iwl_mld_rx_fill_status(struct iwl_mld *mld, int link_id,
 		iwl_mld_rx_eht(mld, skb, phy_data);
 
 #ifdef CPTCFG_IWLWIFI_DEBUGFS
-	if (unlikely(mld->monitor.on))
+	if (unlikely(mld->monitor.on)) {
 		iwl_mld_add_rtap_sniffer_config(mld, skb);
+
+		if (mld->monitor.ptp_time) {
+			u64 adj_time =
+				iwl_mld_ptp_get_adj_time(mld,
+							 phy_data->gp2_on_air_rise *
+							 NSEC_PER_USEC);
+
+			rx_status->mactime = div64_u64(adj_time, NSEC_PER_USEC);
+			rx_status->flag |= RX_FLAG_MACTIME_IS_RTAP_TS64;
+			rx_status->flag &= ~RX_FLAG_MACTIME;
+		}
+	}
 #endif
 
 	if (phy_data->ntfy)
