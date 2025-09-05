1b14094c297eb1f717e97bf565734fe36b8fe8d7
Benjamin Berg <benjamin.berg@intel.com>
2025-09-05 12:37:25 +0200

    backport: add us_to_ktime
    The helper was added upstream in commit d1fd97291423 ("ktime: Add
    us_to_ktime()"), which first appeared in kernel 6.13.
    
    type=feature
    ticket=none
    
    Change-Id: I771da8d603bdc5c50a744cc30745729beb317680
    Signed-off-by: Benjamin Berg <benjamin.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/223948
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: ea68cba56a3b2ecbf866529785c09e01ab108615

---
+++ backport-include/linux/ktime.h
@@ -0,0 +1,13 @@
+#ifndef __BACKPORT_LINUX_KTIME_H
+#define __BACKPORT_LINUX_KTIME_H
+#include <linux/version.h>
+#include_next <linux/ktime.h>
+
+#if LINUX_VERSION_IS_LESS(6,13,0)
+static inline ktime_t us_to_ktime(u64 us)
+{
+	return us * NSEC_PER_USEC;
+}
+#endif /* < 6.13 */
+
+#endif /* __BACKPORT_LINUX_KTIME_H */
