39f99db867f01df4f25e123bced947bbbb7989e5
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2025-09-09 07:38:40 +0300

    [BUGFIX] wifi: iwlwifi: mld: don't allocate a FW ID for NAN_DEVICE vif
    This vif is not added to the FW, so there is no need to allocate a FW id
    for it.
    
    type=bugfix
    ticket=none
    fixes=I5abfab64b4f27bed442aeca6231ffebad979cad6
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: Ie3719b96172d9eac1d2e8d559be8439d18dfcf75
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/224765
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: 004991935f4c1569386f762ac423957392367786

--- drivers/net/wireless/intel/iwlwifi/mld/iface.c
+++ drivers/net/wireless/intel/iwlwifi/mld/iface.c
@@ -392,20 +392,15 @@ static void iwl_mld_mlo_scan_start_wk(struct wiphy *wiphy,
 IWL_MLD_ALLOC_FN(vif, vif)
 
 /* Constructor function for struct iwl_mld_vif */
-static int
+static void
 iwl_mld_init_vif(struct iwl_mld *mld, struct ieee80211_vif *vif)
 {
 	struct iwl_mld_vif *mld_vif = iwl_mld_vif_from_mac80211(vif);
-	int ret;
 
 	lockdep_assert_wiphy(mld->wiphy);
 
 	mld_vif->mld = mld;
 	mld_vif->roc_activity = ROC_NUM_ACTIVITIES;
-
-	ret = iwl_mld_allocate_vif_fw_id(mld, &mld_vif->fw_id, vif);
-	if (ret)
-		return ret;
 
 	if (!mld->fw_status.in_hw_restart) {
 		wiphy_work_init(&mld_vif->emlsr.unblock_tpt_wk,
@@ -420,8 +415,6 @@ iwl_mld_init_vif(struct iwl_mld *mld, struct ieee80211_vif *vif)
 					iwl_mld_mlo_scan_start_wk);
 	}
 	iwl_mld_init_internal_sta(&mld_vif->aux_sta);
-
-	return 0;
 }
 
 int iwl_mld_add_vif(struct iwl_mld *mld, struct ieee80211_vif *vif)
@@ -431,7 +424,13 @@ int iwl_mld_add_vif(struct iwl_mld *mld, struct ieee80211_vif *vif)
 
 	lockdep_assert_wiphy(mld->wiphy);
 
-	ret = iwl_mld_init_vif(mld, vif);
+	iwl_mld_init_vif(mld, vif);
+
+	/* NAN interface type is not known to FW */
+	if (vif->type == NL80211_IFTYPE_NAN)
+		return 0;
+
+	ret = iwl_mld_allocate_vif_fw_id(mld, &mld_vif->fw_id, vif);
 	if (ret)
 		return ret;
 
