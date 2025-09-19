b5da10c278577677d1b014eb8dcf8cb20f10ea0f
Benjamin Berg <benjamin.berg@intel.com>
2025-09-19 09:38:31 +0200

    [BUGFIX] backport: fix version guard of us_to_ktime
    The commit adding us_to_ktime was only included in 6.14, not 6.13.
    Adjust the version guard accordingly.
    
    type=bugfix
    ticket=none
    fixes=I771da8d603bdc5c50a744cc30745729beb317680
    
    Change-Id: Ie8242bf2536346dada8580fe00965f1b91048f2c
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/227372
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 5fd25930c47ca76c04e5fc552bd290d236682171

--- backport-include/linux/ktime.h
+++ backport-include/linux/ktime.h
@@ -3,11 +3,11 @@
 #include <linux/version.h>
 #include_next <linux/ktime.h>
 
-#if LINUX_VERSION_IS_LESS(6,13,0)
+#if LINUX_VERSION_IS_LESS(6,14,0)
 static inline ktime_t us_to_ktime(u64 us)
 {
 	return us * NSEC_PER_USEC;
 }
-#endif /* < 6.13 */
+#endif /* < 6.14 */
 
 #endif /* __BACKPORT_LINUX_KTIME_H */
