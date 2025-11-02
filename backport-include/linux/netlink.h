2db29ad4456bfd4ccb17e323f084949a109e25fc
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2025-11-02 23:28:59 +0200

    wifi: nl80211: define an API for configuring the NAN peer's schedule
    Add an NL80211 command to configure the NAN schedule of a NAN peer.
    Such a schedule contains a list of NAN channels, and a mapping from each
    time slots to the corresponding channel (or unscheduled).
    Also contains more information about the schedule, such as sequence ID
    and map ID.
    
    Not all of the restrictions are validated in this patch. In particular,
    comparison of two maps of the same peer requires storing/retrieving each
    map of each peer, only for validation.
    Therefore, it is the responsibilty of the driver to check that.
    
    type=feature
    ticket=none
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: If0e214ff5b52c9666e985fefa3f7be0ad14d93fb
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/248628
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: dbf71e276b510ee092f7775e471a010f563d9b6c

--- backport-include/linux/netlink.h
+++ backport-include/linux/netlink.h
@@ -14,4 +14,14 @@ static inline void nl_set_extack_cookie_u64(struct netlink_ext_ack *extack,
 }
 #endif
 
+/* NL_SET_ERR_MSG_FMT was added in kernel 6.2, but requires _msg_buf field
+ * in struct netlink_ext_ack which was also added in 6.2. For older kernels,
+ * fall back to NL_SET_ERR_MSG with just the format string (no formatting).
+ */
+#if LINUX_VERSION_IS_LESS(6,2,0)
+#define NL_SET_ERR_MSG_FMT(extack, fmt, args...) \
+NL_SET_ERR_MSG((extack), fmt)
+
+#endif
+
 #endif /* __BACKPORT_LINUX_NETLINK_H */
