7b563029755eab34de7b47150266fbc8e36f4bf2
Nidhish A N <nidhish.a.n@intel.com>
2025-11-06 12:59:30 +0200

    wifi: iwlwifi: mvm: Remove iwl_mvm_mld_can_neg_ttlm
    As part of removal of EMLSR from mvm, remove
    iwl_mvm_mld_can_neg_ttlm as it is no longer
    required.
    
    type=feature
    ticket=jira:WIFI-663096
    
    Signed-off-by: Nidhish A N <nidhish.a.n@intel.com>
    Change-Id: I89b4a85efd172b40c07c10fa938dbbacdcda0e2e
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/236738
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    Reviewed-by: Pagadala Yesu Anjaneyulu <pagadala.yesu.anjaneyulu@intel.com>
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: dad96823ddf291e4bca55a8b3ddf82a0708a2d88

--- drivers/net/wireless/intel/iwlwifi/mvm/mld-mac80211.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/mld-mac80211.c
@@ -890,24 +890,6 @@ static int iwl_mvm_mld_roc(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 	return iwl_mvm_roc_common(hw, vif, channel, duration, type, &ops);
 }
 
-static enum ieee80211_neg_ttlm_res
-iwl_mvm_mld_can_neg_ttlm(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
-			 struct ieee80211_neg_ttlm *neg_ttlm)
-{
-	u16 map;
-	u8 i;
-
-	/* Verify all TIDs are mapped to the same links set */
-	map = neg_ttlm->downlink[0];
-	for (i = 0; i < IEEE80211_TTLM_NUM_TIDS; i++) {
-		if (neg_ttlm->downlink[i] != neg_ttlm->uplink[i] ||
-		    neg_ttlm->uplink[i] != map)
-			return NEG_TTLM_RES_REJECT;
-	}
-
-	return NEG_TTLM_RES_ACCEPT;
-}
-
 const struct ieee80211_ops iwl_mvm_mld_hw_ops = {
 	.tx = iwl_mvm_mac_tx,
 	.wake_tx_queue = iwl_mvm_mac_wake_tx_queue,
@@ -997,6 +979,4 @@ const struct ieee80211_ops iwl_mvm_mld_hw_ops = {
 	.link_sta_add_debugfs = iwl_mvm_link_sta_add_debugfs,
 #endif
 	.set_hw_timestamp = iwl_mvm_set_hw_timestamp,
-
-	.can_neg_ttlm = iwl_mvm_mld_can_neg_ttlm,
 };
