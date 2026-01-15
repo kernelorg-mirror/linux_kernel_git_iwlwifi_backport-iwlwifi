f646fb2854147b5cb2c966d7999bf0f636717aad
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2026-01-15 10:51:43 +0200

    wifi: iwlwifi: don't blindly start the responder upon BSS_CHANGED_FTM_RESPONDER
    mac80211 may just want to stop it, so check the ftm_responder boolean
    before starting the responder.
    
    type=feature
    ticket=none
    
    Change-Id: I1b6922ca8d06d592356d7a5d190e6118fec1d5b5
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/255129
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 27c25f252fd8929884ea24efd09ffc97572344b7

--- drivers/net/wireless/intel/iwlwifi/mld/mac80211.c
+++ drivers/net/wireless/intel/iwlwifi/mld/mac80211.c
@@ -1392,13 +1392,16 @@ iwl_mld_link_info_changed_ap_ibss(struct iwl_mld *mld,
 		iwl_mld_update_beacon_template(mld, vif, link);
 
 	if (changes & BSS_CHANGED_FTM_RESPONDER) {
-		int ret;
-
 		iwl_mld_ftm_responder_clear(mld, vif);
-		ret = iwl_mld_ftm_start_responder(mld, vif, link);
-		if (ret)
-			IWL_WARN(mld, "Failed to enable FTM responder (%d)\n",
-				 ret);
+
+		if (link->ftm_responder) {
+			int ret = iwl_mld_ftm_start_responder(mld, vif, link);
+
+			if (ret)
+				IWL_WARN(mld,
+					 "Failed to enable FTM responder (%d)\n",
+					 ret);
+		}
 	}
 }
 
