cf09ebfff5d9d90e1f9a10fbc0f055fbf8bc3c85
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2026-01-22 12:57:49 +0200

    [BUGFIX] wifi: iwlwifi: mld: fix potential use after free in iwl_mld_remove_link()
    This code frees "link" by calling kfree_rcu(link, rcu_head) and then it
    dereferences "link" to get the "link->fw_id" and the
    "link->rx_omi.finished_work". Free the link at the end of the function
    instead.
    
    type=bugfix
    ticket=none
    fixes=unknown
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: Ie2a0daa3034308dfebe767d9b56cb6507e1f8989
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/257127
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: dd26ad10f2857560f7f66926cff64c5f0caae597

--- drivers/net/wireless/intel/iwlwifi/mld/link.c
+++ drivers/net/wireless/intel/iwlwifi/mld/link.c
@@ -530,15 +530,15 @@ void iwl_mld_remove_link(struct iwl_mld *mld,
 	iwl_mld_rm_link_from_fw(mld, bss_conf);
 	/* Continue cleanup on failure */
 
-	if (!is_deflink)
-		kfree_rcu(link, rcu_head);
-
 	RCU_INIT_POINTER(mld_vif->link[bss_conf->link_id], NULL);
 
 	if (WARN_ON(link->fw_id >= mld->fw->ucode_capa.num_links))
 		return;
 
 	RCU_INIT_POINTER(mld->fw_id_to_bss_conf[link->fw_id], NULL);
+
+	if (!is_deflink)
+		kfree_rcu(link, rcu_head);
 }
 
 void iwl_mld_handle_missed_beacon_notif(struct iwl_mld *mld,
