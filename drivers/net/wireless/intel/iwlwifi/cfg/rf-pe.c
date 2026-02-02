7be7bff22df55cbf1775ec011f4d4a2913872c38
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2026-02-02 10:22:16 +0200

    wifi: iwlwifi: remove nvm_ver for devices that don't need it
    This was needed only to check the NVM for devices that had a specific
    firmware image to run the initial calibrations.
    Remove this field from newer devices that no longer have a specific
    image for those.
    
    type=cleanup
    ticket=none
    
    Change-Id: I5834a3efd0ae7e601a02cc0582287ced405a0aef
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/259737
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: cab4d79219ad0de7fb18340d153aeaa7ed9672f6

--- drivers/net/wireless/intel/iwlwifi/cfg/rf-pe.c
+++ drivers/net/wireless/intel/iwlwifi/cfg/rf-pe.c
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
 /*
- * Copyright (C) 2025 Intel Corporation
+ * Copyright (C) 2025-2026 Intel Corporation
  */
 #include "iwl-config.h"
-
-#define IWL_PE_NVM_VERSION		0x0a1d
 
 /* TODO: set unii9_supported when the firmware will be ready */
 #define IWL_DEVICE_PE							\
@@ -21,7 +19,6 @@
 	.eht_supported = true,						\
 	.uhr_supported = true,						\
 	.num_rbds = IWL_NUM_RBDS_EHT,					\
-	.nvm_ver = IWL_PE_NVM_VERSION,					\
 	.nvm_type = IWL_NVM_EXT
 
 const struct iwl_rf_cfg iwl_rf_pe = {
