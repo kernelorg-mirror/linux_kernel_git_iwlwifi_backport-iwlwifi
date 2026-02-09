a2019b65b668423ffd00d7e5a5e6f2d70d6ca87a
Daniel Gabay <daniel.gabay@intel.com>
2026-02-09 10:38:09 +0200

    wifi: iwlwifi: mld: fix NAN DW end notification handler
    Use IWL_DEBUG_INFO instead of IWL_INFO for logging the DW end
    notification, as this is a recurring event during NAN operation
    and should not spam the kernel log.
    
    Also fix a coding style issue - missing space after 'if'.
    
    type=cleanup
    ticket=none
    fixes=I5abfab64b4f27bed442aeca6231ffebad979cad6
    
    Signed-off-by: Daniel Gabay <daniel.gabay@intel.com>
    Change-Id: Iacd459d0e2df28444bb6ccf8730e2f50440e6e32
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/262026
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: e0e776b95ecfa517bbc04739eeec1f00e1a02127

--- drivers/net/wireless/intel/iwlwifi/mld/nan.c
+++ drivers/net/wireless/intel/iwlwifi/mld/nan.c
@@ -269,9 +269,9 @@ void iwl_mld_handle_nan_dw_end_notif(struct iwl_mld *mld,
 	struct wireless_dev *wdev;
 	struct ieee80211_channel *chan;
 
-	IWL_INFO(mld, "NAN: DW end: band=%u\n", notif->band);
+	IWL_DEBUG_INFO(mld, "NAN: DW end: band=%u\n", notif->band);
 
-	if(IWL_FW_CHECK(mld, !mld_vif, "NAN: DW end without mld_vif\n"))
+	if (IWL_FW_CHECK(mld, !mld_vif, "NAN: DW end without mld_vif\n"))
 		return;
 
 	if (IWL_FW_CHECK(mld, !ieee80211_vif_nan_started(mld->nan_device_vif),
