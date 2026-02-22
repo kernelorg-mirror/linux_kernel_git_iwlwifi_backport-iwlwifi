2262dfae6a120b77f664c6d12f5bdbd86945dccd
Avraham Stern <avraham.stern@intel.com>
2026-02-23 01:27:11 +0200

    wifi: iwlwifi: mld: add handler for NAN ULW attribute notification
    When a notification about a new ULW attribute arrives, send it to
    user space so the ULW attribute can be added to the relevant frames
    (e.g. SDF).
    
    type=feature
    ticket=none
    
    Signed-off-by: Avraham Stern <avraham.stern@intel.com>
    Change-Id: Ide4aaf4553a3980e6990485cd37204a922c36913
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/265743
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: d6cd548744cc31807193a821b3aed0db79d28202

--- drivers/net/wireless/intel/iwlwifi/mld/notif.c
+++ drivers/net/wireless/intel/iwlwifi/mld/notif.c
@@ -348,6 +348,7 @@ CMD_VERSIONS(time_sync_confirm_notif,
 	     CMD_VER_ENTRY(1, iwl_time_msmt_cfm_notify))
 CMD_VERSIONS(ftm_resp_notif, CMD_VER_ENTRY(10, iwl_tof_range_rsp_ntfy))
 CMD_VERSIONS(nan_cluster_notif, CMD_VER_ENTRY(1, iwl_nan_cluster_notif))
+CMD_VERSIONS(nan_ulw_attr_notif, CMD_VER_ENTRY(1, iwl_nan_ulw_attr_notif))
 CMD_VERSIONS(nan_dw_end_notif, CMD_VER_ENTRY(1, iwl_nan_dw_end_notif))
 CMD_VERSIONS(beacon_filter_notif, CMD_VER_ENTRY(2, iwl_beacon_filter_notif))
 
@@ -470,6 +471,8 @@ const struct iwl_rx_handler iwl_mld_rx_handlers[] = {
 			  RX_HANDLER_ASYNC)
 	RX_HANDLER_OF_NAN(MAC_CONF_GROUP, NAN_JOINED_CLUSTER_NOTIF,
 			  nan_cluster_notif)
+	RX_HANDLER_OF_NAN(MAC_CONF_GROUP, NAN_ULW_ATTR_NOTIF,
+			  nan_ulw_attr_notif)
 	RX_HANDLER_OF_NAN(MAC_CONF_GROUP, NAN_DW_END_NOTIF,
 			  nan_dw_end_notif)
 };
