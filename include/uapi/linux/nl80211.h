49dc03fb27ee27b1af94d592b10fc6ad2622300f
Johannes Berg <johannes.berg@intel.com>
2026-02-26 14:29:26 +0100

    wifi: cfg80211: allow devices to advertise extended MLD capa/ops
    For UHR, multi-link power-management capability lives there, and
    so it's needed that hostapd knows what to advertise, and clients
    should have it shown to userspace for information.
    
    Repurpose the existing NL80211_ATTR_ASSOC_MLD_EXT_CAPA_OPS by
    renaming it to NL80211_ATTR_EXT_MLD_CAPA_AND_OPS (with a define
    for compatibility) and advertise the capabilities.
    
    We can also later use the value, if needed, to set per-station
    capabilities on STAs added to AP interfaces.
    
    type=feature
    ticket=jira:WIFI-799595
    
    Change-Id: I378a7c017bfc1ebb072fa8d5d1db2ac9b45596c9
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/267305
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    x-iwlwifi-stack-dev: 1f1020683154f7e25d3f5ee26fa2b8c48be08e6b

--- include/uapi/linux/nl80211.h
+++ include/uapi/linux/nl80211.h
@@ -2980,11 +2980,13 @@ enum nl80211_commands {
  * @NL80211_ATTR_EPCS: Flag attribute indicating that EPCS is enabled for a
  *	station interface.
  *
- * @NL80211_ATTR_ASSOC_MLD_EXT_CAPA_OPS: Extended MLD capabilities and
- *	operations that userspace implements to use during association/ML
- *	link reconfig, currently only "BTM MLD Recommendation For Multiple
- *	APs Support". Drivers may set additional flags that they support
- *	in the kernel or device.
+ * @NL80211_ATTR_EXT_MLD_CAPA_AND_OPS: Extended MLD capabilities and operations.
+ *	For association and link reconfiguration, indicates extra capabilities
+ *	that userspace implements, currently only "BTM MLD Recommendation For
+ *	Multiple APs Support".
+ *	For wiphy information, additional flags that drivers will set, but
+ *	this is informational only for userspace (it's not expected to set
+ *	these.)
  *
  * @NL80211_ATTR_WIPHY_RADIO_INDEX: (int) Integer attribute denoting the index
  *	of the radio in interest. Internally a value of -1 is used to
@@ -3676,7 +3678,7 @@ enum nl80211_attrs {
 	NL80211_ATTR_MLO_RECONF_REM_LINKS,
 	NL80211_ATTR_EPCS,
 
-	NL80211_ATTR_ASSOC_MLD_EXT_CAPA_OPS,
+	NL80211_ATTR_EXT_MLD_CAPA_AND_OPS,
 
 	NL80211_ATTR_WIPHY_RADIO_INDEX,
 
@@ -3728,6 +3730,7 @@ enum nl80211_attrs {
 #define NL80211_ATTR_CSA_C_OFF_BEACON NL80211_ATTR_CNTDWN_OFFS_BEACON
 #define NL80211_ATTR_CSA_C_OFF_PRESP NL80211_ATTR_CNTDWN_OFFS_PRESP
 #define NL80211_ATTR_NAN_INIT_ULW NL80211_ATTR_NAN_ULW
+#define NL80211_ATTR_ASSOC_MLD_EXT_CAPA_OPS NL80211_ATTR_EXT_MLD_CAPA_AND_OPS
 
 /*
  * Allow user space programs to use #ifdef on new attributes by defining them
