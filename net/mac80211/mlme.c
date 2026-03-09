9bec1139eae413e303ce96cb43162e414b4377b8
Lorenzo Bianconi <lorenzo@kernel.org>
2026-03-09 09:28:28 +0100

    [BUGFIX] wifi: mac80211: Remove deleted sta links in ieee80211_ml_reconf_work()
    Delete stale station links announced in the reconfiguration IE
    transmitted by the AP in the beacon frames.
    
    type=bugfix
    ticket=none
    fixes=I4dcf794da2a2d9f4e5f63a4b32158075d27c0660
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Link: https://patch.msgid.link/20260309-mac80211-reconf-remove-sta-link-v2-1-1582aac720c6@kernel.org
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Change-Id: I0735bd02346f989a50acb5115d4cf50f12f5ea7a
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/284857
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    Reviewed-by: Ilan Peer <ilan.peer@intel.com>
    x-iwlwifi-stack-dev: 85e31fdcfa700e7bb4610128467428452b35b8ba

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -7156,6 +7156,7 @@ static void ieee80211_ml_reconf_work(struct wiphy *wiphy,
 		container_of(work, struct ieee80211_sub_if_data,
 			     u.mgd.ml_reconf_work.work);
 	u16 new_valid_links, new_active_links, new_dormant_links;
+	struct sta_info *sta;
 	int ret;
 
 	if (!sdata->u.mgd.removed_links)
@@ -7189,6 +7190,16 @@ static void ieee80211_ml_reconf_work(struct wiphy *wiphy,
 				   "Failed setting active links\n");
 			goto out;
 		}
+	}
+
+	sta = sta_info_get(sdata, sdata->vif.cfg.ap_addr);
+	if (sta) {
+		unsigned long removed_links = sdata->u.mgd.removed_links;
+		unsigned int link_id;
+
+		for_each_set_bit(link_id, &removed_links,
+				 IEEE80211_MLD_MAX_NUM_LINKS)
+			ieee80211_sta_free_link(sta, link_id);
 	}
 
 	new_dormant_links = sdata->vif.dormant_links & ~sdata->u.mgd.removed_links;
