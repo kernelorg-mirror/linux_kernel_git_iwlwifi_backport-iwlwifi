d45a339c235eceb5e4c964b63cc4e4e646d9ff30
Johannes Berg <johannes.berg@intel.com>
2026-03-20 18:16:04 +0100

    wifi: nl80211: check link is beaconing for color change
    When trying to do a color change, validate that the link is
    beaconing first, to avoid calling the driver with an invalid
    link.
    
    type=cleanup
    ticket=none
    
    Change-Id: I128a956a3e610bb6a5c9cad941d7d5b59b735a5e
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/273725
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 6cc9556a15c333b4802416ad5afe9431eb258079

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -18577,6 +18577,10 @@ static int nl80211_color_change(struct sk_buff *skb, struct genl_info *info)
 	params.count = nla_get_u8(info->attrs[NL80211_ATTR_COLOR_CHANGE_COUNT]);
 	params.color = nla_get_u8(info->attrs[NL80211_ATTR_COLOR_CHANGE_COLOR]);
 
+	params.link_id = nl80211_link_id(info->attrs);
+	if (!wdev->links[params.link_id].ap.beacon_interval)
+		return -EINVAL;
+
 	err = nl80211_parse_beacon(rdev, info->attrs, &params.beacon_next,
 				   info->extack);
 	if (err)
@@ -18650,7 +18654,6 @@ static int nl80211_color_change(struct sk_buff *skb, struct genl_info *info)
 			goto out;
 	}
 
-	params.link_id = nl80211_link_id(info->attrs);
 	reg_power =
 		cfg80211_get_6ghz_power_type(params.beacon_color_change.tail,
 					     params.beacon_color_change.tail_len, 0);
