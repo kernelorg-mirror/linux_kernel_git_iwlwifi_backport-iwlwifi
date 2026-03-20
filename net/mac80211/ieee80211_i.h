ddbd8d0159efd17abb113e8e2e0fea1693914cd0
Johannes Berg <johannes.berg@intel.com>
2026-03-20 20:54:08 +0100

    wifi: mac80211: fix per-station PHY capability bandwidth
    When a (link) station connected to an AP interface is not
    capable of EHT, it's possible that the AP interface is in
    160 MHz but the HE channel is narrower, e.g. when EHT has
    puncturing.  In this case, the code doesn't correctly set
    the STAs bandwidth, the station might be capable of using
    160 MHz, but it can't use EHT 160 MHz with puncturing, so
    it must be set to narrower.
    
    Track the AP's 'he_and_lower_bw' bandwidth, use that when
    calculating the maximum bandwidth to transmit to/from any
    station not capable of EHT, and update all stations and
    the chanctx min_def when it changes.
    
    type=feature
    ticket=none
    
    Change-Id: I29ef615e6ab049a56c20f3226b5e953859f890b0
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/273732
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 5a8a3a0407169de4c13852789d140914b54f3788

--- net/mac80211/ieee80211_i.h
+++ net/mac80211/ieee80211_i.h
@@ -1131,6 +1131,10 @@ struct ieee80211_link_data {
 
 	struct ieee80211_bss_conf *conf;
 
+	struct {
+		enum ieee80211_sta_rx_bandwidth he_and_lower;
+	} bss_bw;
+
 #ifdef CPTCFG_MAC80211_DEBUGFS
 	struct dentry *debugfs_dir;
 #endif
