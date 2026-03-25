831caf5cb98cc214bb07949eeba1ca282a9cb8ab
Johannes Berg <johannes.berg@intel.com>
2026-03-25 18:10:13 +0100

    Revert "wifi: mac80211: clean up ieee80211_sta_cap_rx_bw()"
    This reverts commit 3030f03faf24970ccee1071e59e607a3f8a22bd7.
    
    I'm going to do this differently and keep the chandef
    argument, so it can be unified with the TX side.
    
    type=cleanup
    fixes=I2d964967a7bc6fc940aaa5fa8568e99e721a6d60
    ticket=none
    
    Change-Id: Ia27102d0baf873247c4bb819bec00ab6d3a6e00e
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/275478
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: a67ada5b075be7f92f8dbcbea1d59c3d93fa4323

--- net/mac80211/chan.c
+++ net/mac80211/chan.c
@@ -480,8 +480,7 @@ ieee80211_get_sta_bw(struct sta_info *sta, struct ieee80211_link_data *link)
 	 * capabilities here. Calling it RX bandwidth capability is a bit
 	 * wrong though, since capabilities are in fact symmetric.
 	 */
-	width = ieee80211_sta_cap_rx_bw(link_sta,
-					link->conf->chanreq.oper.chan->band);
+	width = _ieee80211_sta_cap_rx_bw(link_sta, &link->conf->chanreq.oper);
 
 	if (width == IEEE80211_STA_RX_BW_20 &&
 	    !link_sta->pub->ht_cap.ht_supported &&
