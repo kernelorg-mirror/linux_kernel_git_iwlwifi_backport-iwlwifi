00590c4b1377b33117c6449a4e80f6aefbef599f
Israel Kozitz <israel.kozitz@intel.com>
2026-04-23 12:26:57 +0300

    wifi: iwlwifi: mld: support FW TLV for NAN max channel switch time
    Add a new FW TLV (IWL_UCODE_TLV_FW_NAN_MAX_CHAN_SWITCH_TIME) that
    allows the firmware to specify the NAN maximum channel switch time
    in microseconds.
    
    When the TLV is present, use its value for the NAN device capability.
    Otherwise, fall back to the default of 4 milliseconds.
    
    type=feature
    ticket=none
    
    Signed-off-by: Israel Kozitz <israel.kozitz@intel.com>
    Change-Id: I15b933407ca3974a65047b63b4f9b00bed3520fb
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/283123
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: e49b8747a7d220dd7e8d75624a5de64bcc78e13d

--- drivers/net/wireless/intel/iwlwifi/iwl-drv.c
+++ drivers/net/wireless/intel/iwlwifi/iwl-drv.c
@@ -1697,6 +1697,12 @@ fw_dbg_conf:
 			capa->num_mcast_key_entries =
 				le32_to_cpup((const __le32 *)tlv_data);
 			break;
+		case IWL_UCODE_TLV_FW_NAN_MAX_CHAN_SWITCH_TIME:
+			if (tlv_len != sizeof(u32))
+				goto invalid_tlv_len;
+			capa->nan_max_chan_switch_time =
+				le32_to_cpup((const __le32 *)tlv_data);
+			break;
 		case IWL_UCODE_TLV_UMAC_DEBUG_ADDRS: {
 			const struct iwl_umac_debug_addrs *dbg_ptrs =
 				(const void *)tlv_data;
