931adc47b5bc775313da77e7d1b64ca1ff893c40
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2026-06-03 13:18:49 +0300

    wifi: iwlwifi: support TTL platform device ID
    Add support for a new device ID that we will have on TTL (sc2).
    
    type=feature
    ticket=none
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: Icc260831e530c1c92c9be615a7077768b1b9ae30
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/296517
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: 6e582a6b1e74d4dde9d85a9a9c14ed2f55ba76f3

--- drivers/net/wireless/intel/iwlwifi/pcie/drv.c
+++ drivers/net/wireless/intel/iwlwifi/pcie/drv.c
@@ -549,6 +549,7 @@ VISIBLE_IF_IWLWIFI_KUNIT const struct pci_device_id iwl_hw_card_ids[] = {
 	{IWL_PCI_DEVICE(0xD340, PCI_ANY_ID, iwl_sc_mac_cfg)},
 	{IWL_PCI_DEVICE(0x6E70, PCI_ANY_ID, iwl_sc_mac_cfg)},
 	{IWL_PCI_DEVICE(0xD240, PCI_ANY_ID, iwl_sc_mac_cfg)},
+	{IWL_PCI_DEVICE(0x9327, PCI_ANY_ID, iwl_sc_mac_cfg)},
 #endif /* CPTCFG_IWLMVM || CPTCFG_IWLMLD */
 
 	{0}
