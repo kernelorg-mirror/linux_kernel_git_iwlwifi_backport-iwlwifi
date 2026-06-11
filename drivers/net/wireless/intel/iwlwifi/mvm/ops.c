31e91df782d482eaf3c85d113b47b65a9fc69c1e
Pagadala Yesu Anjaneyulu <pagadala.yesu.anjaneyulu@intel.com>
2026-06-11 12:57:16 +0300

    wifi: iwlwifi: mvm: add LARI_CONFIG_EXTENSION command
    There is a new UHB extension bitmap that is part of the LARI
    configuration, which needs to be sent to the FW - also frozen ones.
    But in frozen FWs we cannot increase the version of an API, since the
    driver assumes a specific version, depending on the core number.
    In case of a (new) FW that expects the new version and a (old) driver
    that doesn't support that new version, the driver will send a default
    old version, causing a fw assert about its bad size.
    
    To mitigate this, there is a special command which will be supported
    only on those frozen FWs. Old drivers will simply not support/send it,
    and new driver will send it if supported by fw.
    
    type=feature
    ticket=jira:WIFI-885380
    ticket=jira:WIFI-885404
    
    Signed-off-by: Pagadala Yesu Anjaneyulu <pagadala.yesu.anjaneyulu@intel.com>
    Change-Id: I3e32a5d5c9aa13cbb0e599bef630cdb8e3b031c4
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/299126
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: f8ef5793d56c426421c1311945515bab13ba7edb

--- drivers/net/wireless/intel/iwlwifi/mvm/ops.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/ops.c
@@ -735,6 +735,7 @@ static const struct iwl_hcmd_names iwl_mvm_regulatory_and_nvm_names[] = {
 	HCMD_NAME(NVM_GET_INFO),
 	HCMD_NAME(TAS_CONFIG),
 	HCMD_NAME(TX_POWER_LIMIT_OVERRIDE_CMD),
+	HCMD_NAME(LARI_CONFIG_EXTENSION),
 };
 
 /* Please keep this array *SORTED* by hex value.
