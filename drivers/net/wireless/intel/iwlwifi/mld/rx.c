1a812602bee6d53f07ad11e62be4a51130f1596b
Johannes Berg <johannes.berg@intel.com>
2026-06-24 09:15:44 +0200

    [BUGFIX] wifi: iwlwifi: mld: report IM in UHR preamble
    Even when the device isn't capable of reporting the data
    that was actually using interference mitigation (IM) in
    the encoding, it still indicates in the PHY information
    whether or not IM was used, and we may report that. Use
    that bit to indicate IM in radiotap.
    
    type=bugfix
    ticket=none
    fixes=I47fb568ddcd9d6aca634400b2d4850c1fef035ca
    
    Change-Id: I3a65e9851399cef00177f2ac63ef3e8d8f71f4db
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/303044
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 213e0b5b4f177fa6796385fa44008faebf284f84

--- drivers/net/wireless/intel/iwlwifi/mld/rx.c
+++ drivers/net/wireless/intel/iwlwifi/mld/rx.c
@@ -1672,7 +1672,6 @@ static void iwl_mld_decode_uhr_non_tb(struct iwl_mld_rx_phy_data *phy_data,
 				  IEEE80211_RADIOTAP_UHR_KNOWN_PE_DISAMBIGUITY |
 				  IEEE80211_RADIOTAP_UHR_KNOWN_PRI80_CHAN_POS |
 				  IEEE80211_RADIOTAP_UHR_KNOWN_NUMBER_OF_NON_OFDMA_USERS |
-				  /* not supported by HW so IM=1 on is never received */
 				  IEEE80211_RADIOTAP_UHR_KNOWN_INTERFERENCE_MITIGATION);
 
 	uhr->data[0] |= LE32_DEC_ENC(phy_data->ntfy->sigs.uhr.b1,
@@ -1706,6 +1705,8 @@ static void iwl_mld_decode_uhr_non_tb(struct iwl_mld_rx_phy_data *phy_data,
 	uhr->data[7] |= LE32_DEC_ENC(phy_data->ntfy->sigs.uhr.b1,
 				     OFDM_RX_FRAME_UHR_NUM_OF_USERS,
 				     IEEE80211_RADIOTAP_UHR_DATA7_NUMBER_OF_NON_OFDMA_USERS);
+	if (!(phy_data->ntfy->sigs.uhr.b1 & cpu_to_le32(OFDM_RX_FRAME_UHR_IM_DISABLE)))
+		uhr->data[7] |= cpu_to_le32(IEEE80211_RADIOTAP_UHR_DATA7_INTERFERENCE_MITIGATION);
 
 	iwl_mld_uhr_decode_user_ru(phy_data, uhr);
 
