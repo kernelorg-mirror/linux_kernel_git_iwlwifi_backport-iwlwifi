f31154965718a24bbc9ab03271bee7b6ba1752ce
Daniel Gabay <daniel.gabay@intel.com>
2026-07-07 03:39:47 +0300

    Revert "[BUGFIX][NOUPSTREAM] wifi: iwlwifi: remove unii9 support from Pe for now"
    This reverts commit 16039664f3ae81bc9ba29c9c8b154df223837ec1.
    
    UNII-9 support was previously disabled because the firmware was not
    ready. The firmware now supports the feature, but enabling it
    requires both DSM_FUNC_14 and UNEB to be configured accordingly;
    otherwise, UNII-9 remains disabled.
    
    ticket=none
    type=feature
    
    Signed-off-by: Daniel Gabay <daniel.gabay@intel.com>
    Change-Id: Ib5e01a413cee5a239659884f0e7c5ddc09caf6d1
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/306743
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 8dbf13fa4938b5642f47c62e4ba5942d1f6af2c2

--- drivers/net/wireless/intel/iwlwifi/cfg/rf-pe.c
+++ drivers/net/wireless/intel/iwlwifi/cfg/rf-pe.c
@@ -4,7 +4,6 @@
  */
 #include "iwl-config.h"
 
-/* TODO: set unii9_supported when the firmware will be ready */
 #define IWL_DEVICE_PE							\
 	.ht_params = {							\
 		.stbc = true,						\
@@ -16,6 +15,7 @@
 	.non_shared_ant = ANT_B,					\
 	.vht_mu_mimo_supported = true,					\
 	.uhb_supported = true,						\
+	.unii9_supported = true,					\
 	.eht_supported = true,						\
 	.uhr_supported = true,						\
 	.num_rbds = IWL_NUM_RBDS_EHT,					\
