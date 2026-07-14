7eefb2ac0b8a020ce489303e7f43e69b855c451b
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2026-07-14 13:21:36 +0300

    [BUGFIX] wifi: iwlwifi: mvm: remove SRAM access through debugfs
    This is not really secure and we won't really need on old devices
    (driven by iwlmvm) anyway.
    Just remove the code.
    
    type=bugfix
    ticket=jira:WIFI-920956
    fixes=unknown
    
    Change-Id: Ieec87672d84763b599e2d322525f68acff63e701
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/309719
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 6a2e869010f49ebc0f1a6bcccedc809a61cae379

--- drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
+++ drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
@@ -1080,7 +1080,6 @@ struct iwl_mvm {
 	 */
 	struct dentry *debugfs_dir;
 #ifdef CPTCFG_IWLWIFI_DEBUGFS
-	u32 dbgfs_sram_offset, dbgfs_sram_len;
 	u32 dbgfs_prph_reg_addr;
 	bool disable_power_off;
 	bool disable_power_off_d3;
