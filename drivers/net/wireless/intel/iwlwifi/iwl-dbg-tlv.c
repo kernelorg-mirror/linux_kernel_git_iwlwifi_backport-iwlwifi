6d1c38b8d088dd26205523ba52b322e530a23acc
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2026-07-19 07:25:50 +0300

    [BUGFIX] wifi: iwlwifi: Don't allocate a DRAM buffer with an invalid ID
    A DRAM buffer allocation shouldn't have an ID of 0. If a broken TLV
    contained such an id, this buffer will be allocated and a command will
    be sent to the FW. regardless of the FW asserting on that or not, this
    buffer will never be freed,
    since in the free path we do check for INVALID...
    
    type=bugfix
    ticket=none
    fixes=unknown
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: Ib74687a84e65d2e5295d338600283978a7018375
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/310804
    x-iwlwifi-stack-dev: 0b20d2844c6cdf770118f0db0b8bfdcc762b001e

--- drivers/net/wireless/intel/iwlwifi/iwl-dbg-tlv.c
+++ drivers/net/wireless/intel/iwlwifi/iwl-dbg-tlv.c
@@ -647,7 +647,7 @@ static int iwl_dbg_tlv_alloc_fragments(struct iwl_fw_runtime *fwrt,
 	u32 num_frags, remain_pages, frag_pages;
 	int i;
 
-	if (alloc_id < IWL_FW_INI_ALLOCATION_INVALID ||
+	if (alloc_id <= IWL_FW_INI_ALLOCATION_INVALID ||
 	    alloc_id >= IWL_FW_INI_ALLOCATION_NUM)
 		return -EIO;
 
@@ -730,7 +730,7 @@ static int iwl_dbg_tlv_apply_buffer(struct iwl_fw_runtime *fwrt,
 			 IWL_UCODE_TLV_CAPA_DBG_BUF_ALLOC_CMD_SUPP))
 		return 0;
 
-	if (alloc_id < IWL_FW_INI_ALLOCATION_INVALID ||
+	if (alloc_id <= IWL_FW_INI_ALLOCATION_INVALID ||
 	    alloc_id >= IWL_FW_INI_ALLOCATION_NUM)
 		return -EIO;
 
