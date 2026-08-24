fa39573bbf674735403ddb690b74dc36fa6c2668
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2026-08-24 22:09:47 +0300

    [BUGFIX][NOUPSTREAM] wifi: iwlwifi: mld: fix the Rx nss=0 firmware check
    The nss=0 check was added under CPTCFG_IWLWIFI_SUPPORT_DEBUG_OVERRIDE,
    but the Kconfig symbol is CPTCFG_IWLWIFI_SUPPORT_DEBUG_OVERRIDES. Since
    the singular spelling exists nowhere else in the tree, both checks were
    silently compiled out and mac80211 was the only one to complain about
    Rx with nss=0.
    
    While at it, only check nss for VHT and later: iwl_mld_set_rx_rate()
    leaves nss at 0 for CCK, legacy OFDM and HT, so the check would have
    fired on nearly every frame in a sniffer capture and hidden the real
    occurrences. mac80211 doesn't validate nss for those encodings either.
    
    type=bugfix
    ticket=jira:WIFI-929828
    fixes=I5af924946b95da27dd6d49b6cb1d3c19c3e3a961
    
    Change-Id: I038ecb539263e234a8d874658f6c599ee3c0f4ef
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/320463
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 3393341ef7d7ca6fbf63835d7e353f841b47d96d

--- drivers/net/wireless/intel/iwlwifi/mld/rx.c
+++ drivers/net/wireless/intel/iwlwifi/mld/rx.c
@@ -2761,8 +2761,12 @@ void iwl_mld_rx_mpdu(struct iwl_mld *mld, struct napi_struct *napi,
 
 	iwl_mld_rx_fill_status(mld, link_id, hdr, skb, &phy_data);
 
-#ifdef CPTCFG_IWLWIFI_SUPPORT_DEBUG_OVERRIDE
-	IWL_FW_CHECK(mld, !rx_status->nss, "RX nss = 0 at timestamp 0x%x\n",
+#ifdef CPTCFG_IWLWIFI_SUPPORT_DEBUG_OVERRIDES
+	/* nss is only filled in from VHT onwards */
+	IWL_FW_CHECK(mld,
+		     rx_status->encoding != RX_ENC_LEGACY &&
+		     rx_status->encoding != RX_ENC_HT && !rx_status->nss,
+		     "RX nss = 0 at timestamp 0x%x\n",
 		     rx_status->device_timestamp);
 #endif
 	if (iwl_mld_rx_crypto(mld, sta, hdr, rx_status, mpdu_desc, queue,
@@ -2990,8 +2994,12 @@ static void iwl_mld_no_data_rx(struct iwl_mld *mld,
 
 	/* link ID is ignored for NULL header */
 	iwl_mld_rx_fill_status(mld, -1, NULL, skb, &phy_data);
-#ifdef CPTCFG_IWLWIFI_SUPPORT_DEBUG_OVERRIDE
-	IWL_FW_CHECK(mld, !rx_status->nss, "RX nss = 0 at timestamp 0x%x\n",
+#ifdef CPTCFG_IWLWIFI_SUPPORT_DEBUG_OVERRIDES
+	/* nss is only filled in from VHT onwards */
+	IWL_FW_CHECK(mld,
+		     rx_status->encoding != RX_ENC_LEGACY &&
+		     rx_status->encoding != RX_ENC_HT && !rx_status->nss,
+		     "RX nss = 0 at timestamp 0x%x\n",
 		     rx_status->device_timestamp);
 #endif
 
