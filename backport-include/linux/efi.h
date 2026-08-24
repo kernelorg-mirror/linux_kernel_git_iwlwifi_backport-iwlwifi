212d97916863b19fbd765e4fa9cc5067882b9f4d
Pagadala Yesu Anjaneyulu <pagadala.yesu.anjaneyulu@intel.com>
2026-08-24 23:13:35 +0300

    [NOUPSTREAM] backport: define EFI_ACCESS_DENIED for older kernels
    Add the missing EFI_ACCESS_DENIED status definition to the EFI
    compatibility wrapper for kernels predating v6.8.
    
    type=maint
    ticket=none
    
    Signed-off-by: Pagadala Yesu Anjaneyulu <pagadala.yesu.anjaneyulu@intel.com>
    Change-Id: I3a9fe477f3edce796d40866422f85a9888007756
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/320468
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    AI-Code-Review: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: 89cac5ad142947f510aeeba2071e7fe7db2ca0df

--- backport-include/linux/efi.h
+++ backport-include/linux/efi.h
@@ -1,10 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (C) 2022 Intel Corporation
+ * Copyright (C) 2022, 2026 Intel Corporation
  */
 #ifndef __BACKPORT_LINUX_EFI_H
 #define __BACKPORT_LINUX_EFI_H
 #include_next <linux/efi.h>
+
+#ifndef EFI_ACCESS_DENIED
+#define EFI_ACCESS_DENIED	(15 | (1UL << (BITS_PER_LONG - 1)))
+#endif
 
 #ifndef EFI_RT_SUPPORTED_GET_VARIABLE
 #define efi_rt_services_supported(...) efi_enabled(EFI_RUNTIME_SERVICES)
